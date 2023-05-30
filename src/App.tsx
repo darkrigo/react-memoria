becc77b7f5941269ed03f4dc25306b943cec77fe
Your Name <diegorigo2@gmail.com>
2023-05-30 10:37:48 -0300

    adding files to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,155 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+
+import { items } from './data/items'; 
+import { GridItemType } from './types/GridItemType';
+import { GridItem } from './components/GridItem';
+import { formatTimeElapsed } from './components/helpers/formatTimeElapsed';
+
+
+const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showmCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+  
+  useEffect(() => resetAndCreateGrid(), []);
+
+  useEffect(() => {
+    const timer = setInterval(() =>{
+      if(playing){
+        setTimeElapsed(timeElapsed + 1);
+      }
+    },1000);
+    return() => clearInterval(timer)
+  }, [playing, timeElapsed]);
+
+  // verificar se os cards abertos são iguais
+  useEffect(() => {
+    if(showmCount === 2){
+      let opened = gridItems.filter(item => item.shown === true);
+      if(opened.length === 2){
+
+        
+        //1: se são iguais, torna-los permanente
+        let tmpGrid = [...gridItems];
+        if(opened[0].item === opened[1].item){
+            for(let i in tmpGrid){
+              if(tmpGrid[i].shown){
+                tmpGrid[i].permanentShown = true;
+                tmpGrid[i].shown = false;
+              }
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+
+        } else{
+          // 2: se eles sao diferentes, feche os cards
+          setTimeout(() =>{
+            let tmpGrid = [...gridItems];
+            for(let i in tmpGrid){
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          },1000);
+        }
+        
+        setMoveCount(moveCount => moveCount +1);
+      }
+    }
+  }, [showmCount, gridItems]);
+
+  // verificar se o jogo acabou
+  useEffect(() =>{
+    if(moveCount > 0 && gridItems.every(item => item.permanentShown === true)){
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]);
+
+  const resetAndCreateGrid = () => {
+    // Passo 1 - resetar o jogo
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
+
+    // Passo 2 - criar o grid e começar o jogo
+    // 2.1 criar um grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < (items.length * 2); i++) tmpGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false
+      });
+    // 2.2 preencher o grid
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
+    // 2.3 jogar no state
+    setGridItems(tmpGrid);
+
+    // 3 - começar o jogo
+    setPlaying(true);
+  };
+
+  const handleItemClick = (index: number) => {
+    // Lógica para lidar com o clique no item do grid
+    if(playing && index !== null && showmCount < 2){
+      let tmpGrid = [...gridItems];
+
+      if(tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false){
+        tmpGrid[index].shown = true;
+        setShownCount(showmCount + 1);
+      }
+
+      setGridItems(tmpGrid);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label="Tempo" value={formatTimeElapsed(timeElapsed)} />
+          <InfoItem label="Movimento" value={moveCount.toString()} />
+        </C.InfoArea>
+
+        <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+      </C.Info>
+
+      <C.GridArea>
+        <C.Grid>
+          {gridItems.map((item, index)=> (
+            <GridItem 
+              key={index}
+              item={item}
+              onClick={()=>handleItemClick(index)}
+            />
+          ))}
+        </C.Grid>
+        
+      </C.GridArea>
+    </C.Container>
+
+    
   );
-}
+};
 
 export default App;
